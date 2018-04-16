06de346936c2b2cc3b2af99007820d095b4a9399
qqfffff7 <xxy19940318>
2018-04-16 12:26:13 +0800

    测试提交

---
+++ src/priv/xxy/test/HellowWord.java
@@ -0,0 +1,11 @@
+package priv.xxy.test;
+
+/**
+ * xxy -- 2018/4/16.
+ */
+public class HellowWord {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow Word !");
+    }
+}
